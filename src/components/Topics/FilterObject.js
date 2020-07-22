c343f7f2ab51613bb90ba7ecdb9b4edea9590982
Trevor <alutiiq@comcast.net>
2020-07-22 00:04:46 -0700

    work in progress trying to understand

---
+++ src/components/Topics/FilterObject.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+export default class FilterObject extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      employees: [
+        {
+          name: 'Jimmy Joe',
+          title: 'Hack0r',
+          age: 12,
+        },
+        {
+          name: 'Jeremy Schrader',
+          age: 24,
+          hairColor: 'brown'
+        },
+        {
+          name: 'Carly Armstrong',
+          title: 'CEO',
+        }
+      ],
+
+      userInput: '',
+      filteredEmployees: []
+    }
+  }
+  render() {
+    return (
+      <div className="puzzleBox filterObjectPB">
+        <h4> Filter Object </h4>
+        <span className="puzzleText"> Original: { JSON.stringify(this.state.employees, null, 10) } </span>
+        <input className="inputLine"></input>
+        <button className="confirmationButton"> Filter </button>
+        <span className="resultsBox filterObjectRB"> Filtered: { JSON.stringify(this.state.filteredEmployees, null, 10)}</span>
+      </div>
+      
+    )
+  }
+}
