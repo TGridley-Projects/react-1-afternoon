80664315a2115a4e4d5cf87e68dfe3165f4d3fc5
Trevor <alutiiq@comcast.net>
2020-07-26 15:28:35 -0700

    FilterString done

--- src/components/Topics/FilterString.js
+++ src/components/Topics/FilterString.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react';
 
 export default class FilterString extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      toys: ['ball', 'truck', 'bike', 'bubbles', 'elephant', 'phone'],
+      userInput: '',
+      filteredToys: []
+    }
+  }
+
+  handleChange (val){
+    this.setState({userInput: val})
+  }
+
+  filterString (val){
+    let filteredToys = []
+    filteredToys = this.state.toys.filter(function (str) {return str.includes(val)})
+    this.setState({ filteredToys: filteredToys})
+  }
+
   render() {
     return (
-      <p> FilterString Component </p>
+      <div className="puzzleBox filterStrinPB">
+        <h4>Filter String</h4>
+        <span className="puzzleText">Toys: { JSON.stringify(this.state.toys, null, 10)}</span>
+        <input className="inputLine" onChange={(e) => this.handleChange(e.target.value)} />
+        <button className="confirmationButton" onClick={ () => this.filterString(this.state.userInput)}>Filter</button>
+        <span className="reultsBox filterStringPB">Filtered Toys: {JSON.stringify(this.state.filteredToys)}</span>
+      </div>
     )
   }
 }
